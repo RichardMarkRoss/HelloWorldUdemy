0d39364a0c0fd7bfd757f3198c395085c5a6c68b
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-12-29 17:21:49 +0200

    const

--- HelloWorldUdemy/Program.cs
+++ HelloWorldUdemy/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Xml.Linq;
 
 namespace HelloWorldUdemy
 {
@@ -20,6 +19,11 @@ namespace HelloWorldUdemy
         //char = single letter 'A' or unicode
         //string = "name" 
 
+        // constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        // Create a constant of type string with your birthday as its value
+        const string birthday = "29.01.1996";
         static void MyTypesMethod()
         {
             int num1 = 13;
@@ -39,7 +43,8 @@ namespace HelloWorldUdemy
             Console.WriteLine(f1 + " float add by " + f2 + " float is " + f3);
         }
 
-        static void StartingMethod() {
+        static void StartingMethod()
+        {
             var name = "Richard";
             string message = "my name is ";
             string caseName = name.ToUpper();
@@ -61,7 +66,7 @@ namespace HelloWorldUdemy
             //Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.Write("Enter a String and press enter :");
             string readInput = Console.ReadLine();
-            Console.WriteLine("You have entered {0} ",readInput);
+            Console.WriteLine("You have entered {0} ", readInput);
 
             Console.Write("Enter a String and press enter :");
             int asciiValue = Console.Read();
@@ -69,7 +74,8 @@ namespace HelloWorldUdemy
             Console.ReadKey();
             //Console.Clear();
         }
-        static void ImplicitMethod(){
+        static void ImplicitMethod()
+        {
             // impicit conversion
             int num = 12390532;
             long bigNum = num;
@@ -83,7 +89,7 @@ namespace HelloWorldUdemy
             int myInt;
             // explicit conversion
             // cast double to int;
-            myInt = (int) myDouble;
+            myInt = (int)myDouble;
             Console.WriteLine(myInt);
 
             // typeConversion 
@@ -99,7 +105,8 @@ namespace HelloWorldUdemy
             Console.Read();
         }
 
-        static void ParsingStringMethod() {
+        static void ParsingStringMethod()
+        {
             string myString = "15";
             string mySecondString = "13";
             string stringForFloat = "0.85"; // datatype should be float
@@ -117,14 +124,15 @@ namespace HelloWorldUdemy
             Console.Read();
         }
 
-        static void StringManipulationMethod() {
+        static void StringManipulationMethod()
+        {
             int age = 26;
             string name = "Richard";
             string job = "Developer";
 
             //1. string concatenation
             Console.WriteLine("String Concatenation");
-            Console.WriteLine("Hello my name is "+name+" , I am "+age+" years old");
+            Console.WriteLine("Hello my name is " + name + " , I am " + age + " years old");
             Console.WriteLine("\n");
 
             //2. string fromatting
@@ -152,27 +160,54 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
             //instead of using \\ to write files paths we can verbatim string to amke it easier
             //if you remove the @ you will get an error because \U, \A and \D are not calid escape characters
             Console.WriteLine(@"C:\Users\richardr\Pictures\GIT.png");
-            Console.WriteLine(@"You \n have no power here!"); 
+            Console.WriteLine(@"You \n have no power here!");
             Console.WriteLine("you were meant to destroy the sith \n not join them!");
 
             Console.Read();
         }
 
+        static void StringChallengeMethod()
+        {
+            Console.Write("Enter the String here: ");
+            //read first input string
+            string input = Console.ReadLine();
+
+            Console.Write("Enter the Char here: ");
+            //read first input char
+            char searchInput = Console.ReadLine()[0];
+            //get the index of teh character from the string
+            int searchIndex = input.IndexOf(searchInput);
+
+            Console.WriteLine($"Index of character {searchInput} in String is {searchIndex}");
+
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            string lastName = Console.ReadLine();
+
+            string fullname = string.Concat(firstName, " ", lastName);
+            Console.WriteLine($"your full name is {fullname}");
+        }
+
 
         static void Main(string[] args)
         {
-            StringManipulationMethod();
-            Console.WriteLine("===================================");
-            ParsingStringMethod();
-            Console.WriteLine("===================================");
-            ImplicitMethod();
-            Console.WriteLine("===================================");
-            InputTypeMethod();
-            Console.WriteLine("===================================");
-            StartingMethod();
-            Console.WriteLine("===================================");
-            MyTypesMethod();
-            Console.WriteLine("===================================");
+            Console.WriteLine($"My birthday is always going to be: {birthday}");
+            Console.ReadKey();
+            //StringChallengeMethod();
+            //Console.WriteLine("===================================");
+            //StringManipulationMethod();
+            //Console.WriteLine("===================================");
+            //ParsingStringMethod();
+            //Console.WriteLine("===================================");
+            //ImplicitMethod();
+            //Console.WriteLine("===================================");
+            //InputTypeMethod();
+            //Console.WriteLine("===================================");
+            //StartingMethod();
+            //Console.WriteLine("===================================");
+            //MyTypesMethod();
+            //Console.WriteLine("===================================");
             Console.Read();
         }
     }
